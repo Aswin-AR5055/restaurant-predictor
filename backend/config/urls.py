10267f3653d08f6eca64ccff742dfa00ea98af27
Aswin-AR5055 <aswinraj2003@gmail.com>
2026-07-08 08:28:54 +0530

    fix: added a trailing slash for token endpoint in urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("restaurant.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name="token_obtain_pair",),
-    path('api/token/refresh', TokenRefreshView.as_view(), name="token_refresh",),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh",),
     path('api/', include("restaurant.urls"),),
 ]
